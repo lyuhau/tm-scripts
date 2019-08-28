5a9a2410f5f23f7222d83f41c528d8b1473cef4b
Yuhau Lin <Yuhau.Lin@finra.org>
2019-08-27 21:38:03 -0400

    A few more refactors, added comments

--- kittens-game/progress-bar-&-pre-craft.user.js
+++ kittens-game/progress-bar-&-pre-craft.user.js
@@ -12,11 +12,23 @@
   'use strict';
 
   const init = () => {
+    // poll every .1 seconds for the game to be initialized
     if (!game.resPool) {
       setTimeout(init, 100);
       return;
     }
 
+    // searches game.tabs recursively to find anything with a .model
+    var getButtons = () => {
+      var buttons = [game.tabs.find(t => t.tabId == game.ui.activeTabId)]
+        .flatMap(t => [...new Set(Object.keys(t).filter(k => /btn|button|panel|children/i.test(k)).flatMap(k => t[k]))]).filter(Boolean);
+      while (buttons.some(x => !x.model)) {
+        buttons = buttons.flatMap(x => x.model ? x : x.tradeBtn || x.race || x.children).filter(Boolean);
+      }
+      return buttons;
+    }
+
+    // convenience function for calculating state of resources needed
     const extendPrices = prices => prices.map(p => ({
       'name': p.name,
       'have': game.resPool.get(p.name).value,
@@ -26,6 +38,9 @@
       'delta': p.need - p.have,
       'percent': p.have / p.need
     }));
+
+    // used for the pre-craft bar
+    // recursively flattens the price model to find out the total amount of raw resources needed
     const getRawResPrices = priceModels => {
       var rawResPrices = dojo.clone(priceModels);
       while (rawResPrices.some(pm => pm.children)) {
@@ -35,7 +50,10 @@
         .map(e => ({'name': [e[0]], 'val': e[1]}));
       return rawResPrices;
     };
+
     const craftUpTo = (name, count) => game.craft(name, Math.min(count, game.workshop.getCraftAllCount(name)));
+
+    // recursively call pre-craft on craft ingredients
     const preCraft = prices => {
       prices.forEach(e => {
         const delta = e.need - e.have;
@@ -54,6 +72,7 @@
       });
     };
 
+    // function for initializing the status bars and pre-craft buttons (initially with display: none)
     const initButtonExtension = button => {
       const $button = $(button.buttonContent);
       const statusBar = button.statusBar = $('<div class="statusBar"/>')
@@ -92,47 +111,51 @@
       statusBar.append(precraftBar);
     };
 
+    // every .1 seconds, update the buttons
     setInterval(() => {
-      //      $('.btn.nosel').css('overflow', 'hidden');
-      $('.tabInner .btn.nosel:not(.disabled)').find('.statusBar, .precraftBtn').css('display', 'none');
 
-      var buttonsWithPrices = [game.tabs.find(t => t.tabId == game.ui.activeTabId)]
-        .flatMap(t => [...new Set(Object.keys(t).filter(k => /btn|button|panel|children/i.test(k)).flatMap(k => t[k]))]).filter(Boolean);
-      while (buttonsWithPrices.some(x => !x.model)) {
-        buttonsWithPrices = buttonsWithPrices.flatMap(x => x.model ? x : x.tradeBtn || x.race || x.children).filter(Boolean);
-      }
+      // this is supposed to fix the rounded corners, but it also messes up the drop-down menus (the ones for +5, +25, +all, etc.)
+//      $('.btn.nosel').css('overflow', 'hidden');
 
-      buttonsWithPrices.forEach(button => {
+      // set all buttons to hidden
+      $('.tabInner .btn.nosel').find('.statusBar:visible, .precraftBtn:visible').css('display', 'none');
+
+      getButtons()
+        .filter(b => b.model.visible && !b.model.enabled && b.buttonContent.offsetParent)
+        .filter(b => !/\((?:complete|in progress)\)/i.test(b.model.name))
+        .forEach(button => {
         const $button = $(button.buttonContent);
 
         if (!$button.find('.statusBar').length) {
           initButtonExtension(button);
         }
 
-        if (/\((?:complete|in progress)\)/.test(button.model.name)) {
-          return;
-        }
-
+        // don't show the progress/precraft stuff if it's already 100%
         const prices = extendPrices(button.model.prices);
-        const minPercent = Math.max(Math.min(1, ...prices.map(p => p.percent)), .01);
+        const minPercent = Math.max(Math.min(1, ...prices.map(p => p.percent)), .01);  // the 1% minimum is there so you can tell there is a progress bar
         if (minPercent >= 1) {
           return;
         }
 
         button.statusBar.css({'display': 'inline-block'});
+
+        // progress bar
         button.statusBar.progressBar.css({'width': minPercent * 100 + '%'});
 
-        // only show the pre-craft button if at least some of the ingredients are craftable
+        // pre-craft button
+        // only show if at least some of the ingredients are craftable
         if (prices.some(e => e.name != 'wood' && game.workshop.getCraft(e.name))) {
           button.precraftButton.css('display', '');
           button.precraftButton.precraftA.off('click').on('click', e => { e.stopPropagation(); e.preventDefault(); preCraft(prices); return false; });
 
+          // pre-craft bar
           button.controller.fetchExtendedModel(button.model);
           const rawResPrices = getRawResPrices(button.model.priceModels);
           const rawResMinPercent = Math.min(1, ...extendPrices(rawResPrices).map(p => p.percent));
           button.statusBar.precraftBar.css({
             'display': 'inline-block',
             'width': (rawResMinPercent - minPercent) * 100 + '%',
+            // yellow if not 100% pre-craft resources ready, green if it is
             'background-color': rawResMinPercent == 1 ? '#00FF00AA' : '#FFA500AA',
           })
         }
